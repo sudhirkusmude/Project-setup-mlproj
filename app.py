41ab98c9c3e1fb94cf686e946f9e582b6114c0f2
sudhirkusmude <sudhirkusmude1993@gmai.com>
2024-03-08 01:11:06 +0530

    app.py modified

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/greet', methods=['POST'])
+def greet():
+    data = request.get_json()
+
+    if 'name' not in data:
+        return jsonify({'error': 'Name is missing'}), 400
+
+    name = data['name']
+    greeting = f'Hello, {name}!'
+    return jsonify({'greeting': greeting})
+
+if __name__ == '__main__':
+    app.run(debug=True)
